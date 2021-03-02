696898fc06b4ba73d464d5e92b1a7ba461958fa8
Jorge Pérez González <jpgseva@hotmail.com>
2021-03-02 12:29:16 +0100

    Agregar archivos de proyecto.

---
+++ MvcMovie/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MvcMovie.Data;
+using MvcMovie.Models;
+using System;
+
+namespace MvcMovie
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+    }
+}
